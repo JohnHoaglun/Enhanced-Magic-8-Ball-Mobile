fd89e8ce98927d4ea3618e1cfd78256fe8127a78
John Hoaglun <john@hoaglun.com>
2026-09-23 21:28:41 -0500

    Add Xcode project files

---
+++ Enhanced Magic 8 Ball/Enhanced_Magic_8_BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enhanced_Magic_8_BallApp.swift
+//  Enhanced Magic 8 Ball
+//
+//  Created by John Hoaglun on 9/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Enhanced_Magic_8_BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
